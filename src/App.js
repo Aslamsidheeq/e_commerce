2cb8de4c57d7bc115915d963e2a01359ddef9ba7
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-01-25 14:52:35 +0530

    chap84

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import './styles.scss'
-import Directory from'./Components/directory/directory.component'
+import Home from "./routes/home/home.component";
+import {Routes, Route} from 'react-router-dom'
 
-const App = () => {
-    let categories=[
-      {
-        "id": 1,
-        "title": "hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-    ]
-    
-    
-  return (
+const App=()=>{
+  return(
+    <Routes>
 
-   <Directory categories={categories}/>
+      { <Route path='/Home' element={<Home/>}/> 
+      /*if the path string matches the URL, element passed. */}
+    
+    </Routes>
     
 )};
-
 export default App;
