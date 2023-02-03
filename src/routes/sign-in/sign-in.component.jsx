0f6b3b91dffff6bf4189b862ca185090ca3bc146
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-02-03 11:48:24 +0530

    sign in with popup

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
+
 const SignIn = () =>{
+
+    const logGoogleUser = async () =>{
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
     return(
         <div>
             <h1>Sign-In</h1>
+            <button onClick={logGoogleUser}>Sign-In with Google popup</button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,6 +2,16 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+        } from 'firebase/auth'
+
+
+//copied from firebase website
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBHExnPbNYQ72pk_c5LbDgzMTrtl1k0bKM",
@@ -12,5 +22,16 @@ const firebaseConfig = {
   appId: "1:772091541406:web:fb85b4b5b41242557c888f"
 };
 
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+//initialising provider for google auth
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters(
+  {prompt:"select_account"}
+)
+
+export const auth= getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider)
