1dbab965d4b71f16a45347e465ac461622a693a3
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-03-23 19:39:37 +0530

    Buttons

--- src/Components/sign-up-form/sign-up-form.component.jsx
+++ src/Components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import './sign-up-form.styles.scss'
+import './sign-up-form.styles.scss';
+import Button from '../button/button.component'
 import { createAuthUserWithEmailAndPassword,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
     const defaultFormFeilds={
@@ -89,7 +90,7 @@ const SignUpForm = () => {
                     name="confirmPassword" 
                     value={confirmPassword}/>
 
-                <button type="submit">Sign-Up</button> 
+                <Button type="submit">Sign-Up</Button> 
                 {/* 3. on clicking the button, onSubmit function works */}
 
             </form>
