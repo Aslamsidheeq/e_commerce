642fc953884bc6953c3610ae7088fc2cca3f62ed
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-09-11 11:35:08 +0530

    style

--- src/Components/footer/footer.component.jsx
+++ src/Components/footer/footer.component.jsx
@@ -15,15 +15,10 @@ const Footer =()=>{
 				<ul>
 				<h3>Contact us</h3>
 					<li> Customer care : 9988776655</li>
-					<li> Email : aslamsidhik22@gmail.com</li>
+					<li> Email : abc@gmail.com</li>
 					<li> HQ : 221 SOUTH 20TH STREET,
 						SUITE 721 NEW YORK NY 19016</li>
 				</ul>
-			</div>
-			<div className="news">
-				<p>Blog</p>
-				<p>Press</p>
-				<p>Exhibitions</p>
 			</div>
 		</div>
 	)
